e2972455e6aeb4d91fe26982150f5b74a760c768
Bruno Marques <brunomarques@Brunos-MacBook-Pro.local>
2016-05-05 12:04:24 -0400

    yo

--- server.js
+++ server.js
@@ -2,7 +2,10 @@
 // =============================================================
 var express = require('express');
 var bodyParser = require('body-parser');
-var path = require('path');
+var path = require('path'); 
+var api_Route = require('./app/routing/api-routes.js');
+var html_Route = require('./app/routing/html-routes.js');
+var logger = require("morgan");
 
 // Sets up the Express App
 // =============================================================
@@ -13,4 +16,15 @@ var PORT = 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
-app.use(bodyParser.json({type:'application/vnd.api+json'}));
+app.use(bodyParser.json({type:'application/vnd.api+json'})); 
+app.use(logger('dev'));
+
+
+app.get('/api/friends', apiRoute);
+app.get('/survey', htmlRoute);
+app.use('/', htmlRoute);
+
+
+app.listen(PORT, function(){
+	console.log('App listening on PORT ' + PORT);
+});
